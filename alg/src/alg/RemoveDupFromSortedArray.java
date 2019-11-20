5f5f28d6e4523bbc0a7c4adc5d40bdf234a27f51
梅浩 <752742999@qq.com>
2019-11-20 19:58:52 +0800

    提交

--- alg/src/alg/RemoveDupFromSortedArray.java
+++ alg/src/alg/RemoveDupFromSortedArray.java
@@ -2,7 +2,26 @@ package alg;
 
 public class RemoveDupFromSortedArray {
     
-    
+    public int removeDuplicates(int[] nums) {
+        int newNumslength = nums.length;
+        if (newNumslength == 1)
+            return 1;       
+        
+        int newIndex = 0;
+        int change = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            int j = i + 1;
+            if (nums[i] == nums[j]) {
+                change = j;
+                newNumslength --;    
+            } else {
+                if (j > change) {
+                    nums[newIndex = (newIndex + 1)] = nums[j];
+                }
+            }
+        }        
+        return newNumslength;
+    }
     
     
 
